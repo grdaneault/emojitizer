b1bbcab26e8f3805040701cb05de1a8b241bb7d6
Greg Daneault <gdaneault@carbonblack.com>
2018-08-27 11:17:06 -0400

    Download custom emoji using the slack api list rather than a custom json blob

--- custom_emoji/download_emoji.py
+++ custom_emoji/download_emoji.py
@@ -1,19 +1,29 @@
 import json
 import glob
 import requests
+import os
+from slackclient import SlackClient
 
 
 def download_file(url, name):
-    resp = requests.get(url, allow_redirects=True)
     file_type = url.split(".")[-1]
-    name = "img/{}.{}".format(name, file_type)
-    open(name, 'wb').write(resp.content)
+    file_name = "img/{}.{}".format(name, file_type)
+    if not os.path.exists(file_name):
+        print("Downloading {} as {}... ".format(name, file_name), end="")
+        resp = requests.get(url, allow_redirects=True)
+        open(file_name, 'wb').write(resp.content)
+        print("done.")
+    else:
+        print("Skipping {} as {}...".format(name, file_name))
 
 
-for config in glob.glob("*.json"):
-    if config != "emoji.json":
-        config = json.load(open(config))
-        for emoji in config:
-            emoji['name'] = emoji['name'].strip(":")
-            print("{name}: {url}".format(**emoji))
-            download_file(**emoji)
+client = SlackClient(os.environ.get('SLACK_TOKEN_2'))
+
+emoji = client.api_call("emoji.list")
+
+for name in emoji['emoji']:
+    url = emoji['emoji'][name]
+    if url.startswith("alias"):
+        continue
+    else:
+        download_file(url, name)
